66668b6bd32b184ffad96c75ec1b6e6a120418bd
maxLolPy <64307135+maxLolPy@users.noreply.github.com>
2020-04-25 12:40:50 +0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-wget -O raspisanie.py https://vk.com/away.php?utf=1&to=https%3A%2F%2Fyadi.sk%2Fd%2FSjFQaDJI3bG_Xw
 sudo apt update --fix-missing
 sudo apt upgrade
 sudo apt install python3-pip
